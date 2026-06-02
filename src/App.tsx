e0e5ec327986fc73693185beb576980d78962cbd
Developer-manas <manaskumar9102@gmail.com>
2026-06-03 01:27:48 +0530

    added signin/signup + setting attributes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import View from './pages/View'
 import Navbar from './components/Navbar'
 import { Toaster, toast } from 'sonner'
 import AuthPage from './pages/auth/AuthPage'
+import Settings from './pages/Settings'
+
 
 
 const App = () => {
@@ -33,6 +35,7 @@ const App = () => {
         <Route path="/community" element={<Community />} />
         <Route path="/view/:projectId" element={<View  />} />
         <Route path="/auth/:pathname" element={<AuthPage />} />
+        <Route path="/account/settings" element={<Settings />} />
       </Routes>
     </div>
   )
